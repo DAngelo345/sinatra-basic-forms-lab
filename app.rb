c598372175971ec1b0c6b99a4a4a9ac81fcf1f39
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-04-21 17:58:08 -0400

    commit tree 62b51b9d38c3579847d767febeed1a0378b856a7

--- app.rb
+++ app.rb
@@ -2,4 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+
+        erb :'//index'
+    end
+
+    get '/new' do 
+       
+
+        erb :'create_puppy'
+    end
+    
+    post '/puppy' do
+        pname = params[:name]
+        pbreed = params[:breed]
+        page = params[:age]
+        # binding.pry
+        @puppy = Puppy.new(pname, pbreed, page)
+       
+    
+
+
+        erb :'display_puppy'
+    end
+
 end
